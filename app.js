e05f9731035811a2acca296a94e60aa2e5b95569
Beknazar Suranchiyev <Suranchiyev96@gmail.com>
2018-09-22 22:58:38 -0400

    Added a Procfile.

--- app.js
+++ app.js
@@ -54,4 +54,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
 module.exports = app;
